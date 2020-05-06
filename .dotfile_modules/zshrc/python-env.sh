41c18393bc673cde927b72531724c65418c20f0e
Jaewon Chung <jaywonchung@snu.ac.kr>
2020-05-07 02:41:06 +0900

    Modularize python environment

---
+++ .dotfile_modules/zshrc/python-env.sh
@@ -0,0 +1,22 @@
+#  # >>> conda initialize >>>
+#  # !! Contents within this block are managed by 'conda init' !!
+#  __conda_setup="$("$HOME/anaconda3/bin/conda" 'shell.zsh' 'hook' 2> /dev/null)"
+#  if [ $? -eq 0 ]; then
+#      eval "$__conda_setup"
+#  else
+#      if [ -f "$HOME/anaconda3/etc/profile.d/conda.sh" ]; then
+#          . "$HOME/anaconda3/etc/profile.d/conda.sh"
+#      else
+#          export PATH="$HOME/anaconda3/bin:$PATH"
+#      fi
+#  fi
+#  unset __conda_setup
+#  # <<< conda initialize <<<
+#  
+#  # Deactivate conda base environment
+#  conda deactivate
+#  
+#  # pyenv
+#  export PATH="$HOME/.pyenv/bin:$PATH"
+#  eval "$(pyenv init -)"
+#  eval "$(pyenv virtualenv-init -)"
