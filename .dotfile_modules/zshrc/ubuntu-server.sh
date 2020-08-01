a953bf6c8ce9e7914fdf737b1e2ac2a8904b40ef
Jaewon Chung <jaywonchung@snu.ac.kr>
2020-08-02 01:40:10 +0900

    Set GPU

--- .dotfile_modules/zshrc/ubuntu-server.sh
+++ .dotfile_modules/zshrc/ubuntu-server.sh
@@ -3,6 +3,10 @@
 export PATH="/usr/local/cuda/bin:$PATH"
 export LD_LIBRARY_PATH="/usr/local/cuda/lib64:$LD_LIBRARY_PATH"
 
+# Assigned GPU
+export CUDA_DEVICE_ORDER=PCI_BUS_ID
+export CUDA_VISIBLE_DEVICES=5
+
 # Crane
 export PATH="$HOME/workspace/crane/bin:$PATH"
 
