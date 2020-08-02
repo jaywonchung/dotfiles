649cb890257f92757f226460c6ec596fff47ae7e
Jaewon Chung <jaywonchung@snu.ac.kr>
2020-08-03 00:05:36 +0900

    Track other branches

--- .install_dotfiles/init.sh
+++ .install_dotfiles/init.sh
@@ -2,3 +2,4 @@
 
 alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME' # later done in .zshrc
 dotfiles config --local status.showUntrackedFiles no # settings kept in .dotfiles/config
+dotfiles config --local remote.origin.fetch '+refs/heads/*:refs/remotes/origin/*' # track other branches too
