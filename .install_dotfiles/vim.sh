0aa965441cd2b754b66649e30da2428a0120a0cd
Jaewon Chung <jaywonchung@snu.ac.kr>
2020-09-05 17:06:55 +0900

    Better vim install script

--- .install_dotfiles/vim.sh
+++ .install_dotfiles/vim.sh
@@ -1,5 +1,9 @@
 # install Vundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
+# clone syntastic
+git clone --depth=1 https://github.com/vim-syntastic/syntastic.git ~/.vim/bundle/vim-syntastic
+
 # install plugins
-vim +PluginInstall +Helptags +qall!
+echo 'Install vim plugins. This will take a while.'
+vim -E -s -u ~/.vimrc +PluginInstall +Helptags +qall!
