c44cd7bacaa7400e06101f8839ff7363e26f4c6b
Jaewon Chung <jaywonchung@snu.ac.kr>
2020-10-02 22:35:13 +0900

    Update .install_dotfiles/vim.sh

--- .install_dotfiles/vim.sh
+++ .install_dotfiles/vim.sh
@@ -3,5 +3,5 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 # install plugins
-echo 'Install vim plugins. This will take a while.'
-vim -E -s -u ~/.vimrc +PlugInstall +Helptags +qall!
+echo 'Installing vim plugins.'
+vim -E -s -u ~/.vimrc +PlugInstall +qall!
