fc13bee10a9ab379f471842171b524b48b2c2931
Jaewon Chung <jaywonchung@snu.ac.kr>
2020-10-03 01:09:31 +0900

    All to nvim

--- .dotfile_modules/zshrc/ubuntu-server.sh
+++ .dotfile_modules/zshrc/ubuntu-server.sh
@@ -1,4 +1,10 @@
 # Machine-specific Ubuntu server with CUDA
+# Neovim
+if ! command -v nvim &> /dev/null; then
+    mkdir -p ~/.local/bin
+    ln -s $(which vim) ~/.local/bin/nvim
+fi
+
 # NVCC
 export PATH="/usr/local/cuda/bin:$PATH"
 export LD_LIBRARY_PATH="/usr/local/cuda/lib64:$LD_LIBRARY_PATH"
