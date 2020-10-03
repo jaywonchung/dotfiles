f761d2370ef29386a0e0c52501b58796cf7eef4a
Jae-Won Chung <jaywonchung@snu.ac.kr>
2020-10-04 03:21:00 +0900

    Aggregate to .dotmodules

---
+++ .dotmodules/install/install.sh
@@ -0,0 +1,9 @@
+#!/bin/zsh
+
+set -e
+
+source ~/.dotmodules/install/util.sh
+source ~/.dotmodules/install/zsh.sh
+source ~/.dotmodules/install/vim.sh
+source ~/.dotmodules/install/tmux.sh
+source ~/.dotmodules/install/font.sh
--- .dotmodules/install/tmux.sh
+++ .dotmodules/install/tmux.sh
@@ -1,10 +1,10 @@
 #!/bin/zsh
 
 installing "terminfo tmux-256color"
-TERMINFO=~/.terminfo tic -x .dotfile_modules/tmux/terminfo
+TERMINFO=~/.terminfo tic -x .dotmodules/tmux/terminfo
 
 installing "TPM"
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+git clone --depth=1 https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 installing "TPM plugins"
 ~/.tmux/plugins/tpm/bin/install_plugins
