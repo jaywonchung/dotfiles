f727bb84f728dc1699ad8d8e97035837daaaa6a7
Jaewon Chung <jaywonchung@snu.ac.kr>
2020-10-03 18:17:25 +0900

    Refactor install scripts

--- .install_dotfiles/vim.sh
+++ .install_dotfiles/vim.sh
@@ -1,7 +1,15 @@
-# install vim-plug
+#!/bin/zsh
+
+installing "neovim"
+curl -LO https://github.com/neovim/neovim/releases/download/nightly/nvim-linux64.tar.gz
+tar xzvf nvim-linux64.tar.gz
+mkdir -p ~/.local
+rsync -a nvim-linux64/* ~/.local/
+rm -rf nvim-linux64
+
+installing "vim-plug"
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-# install plugins
-echo 'Install vim plugins. This will take a while.'
-vim -E -s -u ~/.vimrc +PlugInstall +Helptags +qall!
+installing "vim plugins"
+vim -E -s -u ~/.vimrc +PlugInstall +qall!
--- .install_dotfiles/zsh.sh
+++ .install_dotfiles/zsh.sh
@@ -1,26 +1,28 @@
 #!/bin/zsh
 
-# zsh
+installing "Oh-my-zsh"
+export RUNZSH=no
+export KEEP_ZSHRC=yes
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+source ~/.zshrc
 
-# install powerlevel10k
+installing "Powerlevel10k"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "$ZSH_CUSTOM/themes/powerlevel10k"
 
-# install fast-syntax-highlighting
-git clone https://github.com/zdharma/fast-syntax-highlighting.git "$ZSH_CUSTOM/plugins/fast-syntax-highlighting"
+installing "fast-syntax-highlighting"
+git clone --depth=1 https://github.com/zdharma/fast-syntax-highlighting.git "$ZSH_CUSTOM/plugins/fast-syntax-highlighting"
 
-# install zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-autosuggestions "$ZSH_CUSTOM/plugins/zsh-autosuggestions"
+installing "zsh-autosuggestions"
+git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions.git "$ZSH_CUSTOM/plugins/zsh-autosuggestions"
 
-# install fuzzy finder
+installing "fzf"
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-~/.fzf/install
+printf 'y\ny\nn\n' | ~/.fzf/install # Enable fuzzy-autocompletion and key bindings. Do not modify .zshrc.
 
-# install autojump
-git clone https://github.com/wting/autojump.git ~/_autojump
-cd ~/_autojump
-./install.py
+installing "autojump"
+git clone --depth=1 https://github.com/wting/autojump.git ~/_autojump
+~/_autojump/install.py
 rm -rf ~/_autojump
 
-# install direnv
+installing "direnv"
 curl -sfL https://direnv.net/install.sh | bin_path=~/.local/bin bash
