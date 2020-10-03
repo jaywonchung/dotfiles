8e33292571a42c61430503c4b1b7d31f2997d831
Jae-Won Chung <jaywonchung@snu.ac.kr>
2020-10-04 03:18:03 +0900

    Aggregate to .dotmodules

---
+++ .dotmodules/install/install.sh
@@ -0,0 +1,8 @@
+#!/bin/zsh
+
+set -e
+
+source ~/.dotmodules/install/util.sh
+source ~/.dotmodules/install/zsh.sh
+source ~/.dotmodules/install/vim.sh
+source ~/.dotmodules/install/tmux.sh
--- .install_dotfiles/install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/zsh
-
-set -e
-
-source ~/.install_dotfiles/util.sh
-source ~/.install_dotfiles/zsh.sh
-source ~/.install_dotfiles/vim.sh
-source ~/.install_dotfiles/tmux.sh
