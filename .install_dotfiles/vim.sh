64efa847e066ebcfe4937ffe1e5ba3bb26187771
Jae-Won Chung <jaywonchung@snu.ac.kr>
2020-10-03 18:22:30 +0900

    Refactor install scripts

--- .install_dotfiles/vim.sh
+++ .install_dotfiles/vim.sh
@@ -1,11 +1,11 @@
 #!/bin/zsh
 
 installing "neovim"
-curl -LO https://github.com/neovim/neovim/releases/download/nightly/nvim-linux64.tar.gz
-tar xzvf nvim-linux64.tar.gz
+curl -LO https://github.com/neovim/neovim/releases/download/nightly/nvim-macos.tar.gz
+tar xzvf nvim-macos.tar.gz
 mkdir -p ~/.local
-rsync -a nvim-linux64/* ~/.local/
-rm -rf nvim-linux64
+rsync -a nvim-macos/* ~/.local/
+rm -rf nvim-macos
 
 installing "vim-plug"
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
