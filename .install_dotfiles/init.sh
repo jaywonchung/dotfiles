c52bd3051e5d5d9c7cda9614fc328677dc34dae0
Jaewon Chung <jaywonchung@snu.ac.kr>
2020-10-05 16:42:10 +0900

    Use neovim as difftool

--- .install_dotfiles/init.sh
+++ .install_dotfiles/init.sh
@@ -1,5 +1,7 @@
 #!/bin/zsh
 
-alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME' # later done in .zshrc
-dotfiles config --local status.showUntrackedFiles no # settings kept in .dotfiles/config
-dotfiles config --local remote.origin.fetch '+refs/heads/*:refs/remotes/origin/*' # track other branches too
+alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'  # later done in .zshrc
+dotfiles config --local status.showUntrackedFiles no  # settings kept in .dotfiles/config
+dotfiles config --local remote.origin.fetch '+refs/heads/*:refs/remotes/origin/*'  # track other branches too
+dotfiles config --local diff.tool nvim  # use neovim as difftool
+dotfiles config --local difftool.nvim.cmd 'nvim -d $LOCAL $REMOTE'  # use neovim as difftool
