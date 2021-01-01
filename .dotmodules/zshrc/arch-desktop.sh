24bb122d0399ef9bcd894f4d2228bc6e2631e9a3
Jae-Won Chung <jaywonchung@snu.ac.kr>
2021-01-01 22:47:18 +0900

    Update machine-specific.sh

--- .dotmodules/zshrc/arch-desktop.sh
+++ .dotmodules/zshrc/arch-desktop.sh
@@ -1,4 +1,9 @@
 # Machine-specific Arch Linux desktop
+# Kitty
+if [ "$TERM" = "xterm-kitty" ]; then
+  alias icat='kitty +kitten icat'
+fi
+
 # NVCC
 export PATH="/usr/local/cuda/bin:$PATH"
 export LD_LIBRARY_PATH="/usr/local/cuda/lib64:$LD_LIBRARY_PATH"
