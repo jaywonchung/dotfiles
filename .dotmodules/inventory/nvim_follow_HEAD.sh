5b1fd62c525eac7160b4bde2d357efde40dfd0e5
Jae-Won Chung <jaywonchung@snu.ac.kr>
2021-01-10 16:15:27 +0900

    Add cleanup

--- .dotmodules/inventory/nvim_follow_HEAD.sh
+++ .dotmodules/inventory/nvim_follow_HEAD.sh
@@ -28,3 +28,6 @@ rsync -a $INSTALL_DIR/* "$HOME/.local/"
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 nvim -E -s -u ~/.config/nvim/init.vim +PlugInstall +qall!
+
+# Cleanup
+rm -rf "$INSTALL_DIR"
