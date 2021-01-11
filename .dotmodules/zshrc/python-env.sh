71180046887f2ecfb746291fc7b5e71495edc60f
Jae-Won Chung <jaywonchung@snu.ac.kr>
2021-01-12 01:02:22 +0900

    Move anaconda to .local

--- .dotmodules/zshrc/python-env.sh
+++ .dotmodules/zshrc/python-env.sh
@@ -1,13 +1,13 @@
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
-__conda_setup="$("$HOME/anaconda3/bin/conda" 'shell.zsh' 'hook' 2> /dev/null)"
+__conda_setup="$("$HOME/.local/anaconda3/bin/conda" 'shell.zsh' 'hook' 2> /dev/null)"
 if [ $? -eq 0 ]; then
     eval "$__conda_setup"
 else
-    if [ -f "$HOME/anaconda3/etc/profile.d/conda.sh" ]; then
-        . "$HOME/anaconda3/etc/profile.d/conda.sh"
+    if [ -f "$HOME/.local/anaconda3/etc/profile.d/conda.sh" ]; then
+        . "$HOME/.local/anaconda3/etc/profile.d/conda.sh"
     else
-        export PATH="$HOME/anaconda3/bin:$PATH"
+        export PATH="$HOME/.local/anaconda3/bin:$PATH"
     fi
 fi
 unset __conda_setup
