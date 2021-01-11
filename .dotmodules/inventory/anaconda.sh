601af22ff56cdb179781a625022c0bf5a3f50991
Jae-Won Chung <jaywonchung@snu.ac.kr>
2021-01-12 01:02:10 +0900

    Update anaconda.sh

--- .dotmodules/inventory/anaconda.sh
+++ .dotmodules/inventory/anaconda.sh
@@ -6,4 +6,4 @@ VERSION=${VERSION:-2020.11}
 
 cd /tmp
 curl -O "https://repo.anaconda.com/archive/Anaconda3-$VERSION-Linux-x86_64.sh"
-bash Anaconda3-*
+bash "Anaconda3-$VERSION-Linux-x86_64.sh" -b -p "$HOME/.local/anaconda3" 
