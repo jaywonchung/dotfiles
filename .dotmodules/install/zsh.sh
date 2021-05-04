65c6e8f046c5451a02d14c3bd4d9b96872ffb52c
Jae-Won Chung <jaywonchung@snu.ac.kr>
2021-05-04 13:42:42 +0900

    Fix install scripts

--- .dotmodules/install/zsh.sh
+++ .dotmodules/install/zsh.sh
@@ -14,7 +14,7 @@ installing "Oh-my-zsh"
 export RUNZSH=no
 export KEEP_ZSHRC=yes
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-source ~/.zshrc
+source ~/.zshrc || true
 
 installing "Powerlevel10k"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "$ZSH_CUSTOM/themes/powerlevel10k"
