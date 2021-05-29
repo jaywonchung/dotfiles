0deb89954397e1a8a492f92e83748306cdc8f6c0
Jae-Won Chung <jaywonchung@snu.ac.kr>
2021-05-29 19:21:55 +0900

    Fix pyright.sh

--- .dotmodules/inventory/pyright.sh
+++ .dotmodules/inventory/pyright.sh
@@ -5,6 +5,7 @@ VERSION=${VERSION:-"14.16.0"}
 cd /tmp
 curl -LO "https://nodejs.org/dist/v$VERSION/node-v$VERSION-linux-x64.tar.xz"
 tar xJf "node-v$VERSION-linux-x64.tar.xz" -C ~/.local/src
+mkdir -p ~/.local/src
 mv ~/.local/src/node* ~/.local/src/node
 
 npm install -g pyright
