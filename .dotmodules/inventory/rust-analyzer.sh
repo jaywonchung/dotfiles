c11fbc302e108b997d0ef65ce9af25fde5419437
Jae-Won Chung <jaywonchung@snu.ac.kr>
2021-06-14 23:27:20 +0900

    Fix rust-analyzer.sh

--- .dotmodules/inventory/rust-analyzer.sh
+++ .dotmodules/inventory/rust-analyzer.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-curl -L https://github.com/rust-analyzer/rust-analyzer/releases/latest/download/rust-analyzer-linux \
-     -o ~/.local/bin/rust-analyzer
-chmod +x ~/.local/bin/rust-analyzer
+cd /tmp
+curl -LO https://github.com/rust-analyzer/rust-analyzer/releases/latest/download/rust-analyzer-x86_64-unknown-linux-gnu.gz
+gzip -d rust-analyzer-x86_64-unknown-linux-gnu.gz
+mv rust-analyzer-x86_64-unknown-linux-gnu rust-analyzer
+chmod +x rust-analyzer
+mv rust-analyzer ~/.local/bin
