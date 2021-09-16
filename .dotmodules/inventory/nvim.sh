e13dad6cda6974e3db6b2a7f5c45459fed58b617
Jae-Won Chung <jwnchung@umich.edu>
2021-09-16 11:38:04 -0400

    Create nvim.sh

---
+++ .dotmodules/inventory/nvim.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -ev
+
+VERSION=${2:-"v0.5.0"}
+
+# Get nightly release
+cd /tmp
+curl -LO "https://github.com/neovim/neovim/releases/download/$VERSION/nvim-linux64.tar.gz"
+tar xzf nvim-linux64.tar.gz
+mkdir -p ~/.local
+
+# Remove
+rm -f ~/.local/bin/nvim    || true
+rm -rf ~/.local/lib/nvim   || true
+rm -rf ~/.local/share/nvim || true
+
+# Install nvim
+rsync -a nvim-linux64/* ~/.local/
+
+# Install plugins
+sh -c 'curl -fLo "$HOME/.local/share/nvim/site/autoload/plug.vim" --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+nvim -E -s -u ~/.config/nvim/init.vim +PlugInstall +qall!
