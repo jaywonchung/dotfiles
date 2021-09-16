85f13a78a781824474c64d0316d9a21d79ec17e6
Jae-Won Chung <jwnchung@umich.edu>
2021-09-16 10:55:18 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
 git clone --bare https://github.com/jaywonchung/dotfiles.git $HOME/.dotfiles
 git --git-dir=$HOME/.dotfiles --work-tree=$HOME checkout master
 source .dotmodules/init.sh
+
+dotfiles checkout ubuntu-server
 zsh $HOME/.dotmodules/install/all.sh
