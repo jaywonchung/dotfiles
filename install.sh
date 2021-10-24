4453895ac4ea73e04c8df91797756db77c63db0f
Jae-Won Chung <jwnchung@umich.edu>
2021-10-23 21:59:36 -0400

    Add SSH=1 option to install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,8 @@
-git clone --bare https://github.com/jaywonchung/dotfiles.git $HOME/.dotfiles
+if [ -n "$SSH" ]; then
+  git clone --bare git@github.com:jaywonchung/dotfiles $HOME/.dotfiles
+else
+  git clone --bare https://github.com/jaywonchung/dotfiles.git $HOME/.dotfiles
+fi
 git --git-dir=$HOME/.dotfiles --work-tree=$HOME checkout master
 source .dotmodules/init.sh
 
