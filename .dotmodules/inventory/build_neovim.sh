71d7f85cdeb7bb270d5cf1df2ddb9c66061f23f2
Jae-Won Chung <jwnchung@umich.edu>
2023-02-22 01:20:57 -0500

    Add build_install_nvim.sh

--- .dotmodules/inventory/build_neovim.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/local/env bash
-
-set -e
-
-docker run --rm -u $(id -u) -v $HOME/.local/src/neovim:/workspace/neovim -v $HOME/.local:$HOME/.local jw-neovim:latest bash -c "cd /workspace/neovim; git checkout stable; make distclean; make deps; make CMAKE_BUILD_TYPE=Release CMAKE_INSTALL_PREFIX=$HOME/.local && make CMAKE_INSTALL_PREFIX=$HOME/.local install"
