af7873c0667aa32c17c79f7585b27286a8a3dda2
Jae-Won Chung <jwnchung@umich.edu>
2023-06-26 00:24:55 -0400

    Update bat version

--- .dotmodules/inventory/bat.sh
+++ .dotmodules/inventory/bat.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VERSION=${VERSION:-"0.16.0"}
+VERSION=${VERSION:-"0.23.0"}
 
 cd /tmp
 curl -LO "https://github.com/sharkdp/bat/releases/download/v$VERSION/bat-v$VERSION-x86_64-unknown-linux-gnu.tar.gz"
