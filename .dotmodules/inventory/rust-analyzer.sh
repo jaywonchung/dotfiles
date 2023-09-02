ccaa8c4b41e94ca2123a7daa07cab6658b5d60d5
Jae-Won Chung <jwnchung@umich.edu>
2023-09-01 22:56:37 -0400

    Fix rust-analyzer install script

--- .dotmodules/inventory/rust-analyzer.sh
+++ .dotmodules/inventory/rust-analyzer.sh
@@ -6,6 +6,8 @@ else
   ARCH=x86_64
 fi
 
+rm ~/.local/bin/rust-analyzer || true
+
 curl -L https://github.com/rust-analyzer/rust-analyzer/releases/latest/download/rust-analyzer-$ARCH-apple-darwin.gz \
   | gunzip -c - > ~/.local/bin/rust-analyzer
 chmod +x ~/.local/bin/rust-analyzer
