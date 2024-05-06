476c0b63345ab485ac411ebd5939fb734d8d5092
Jae-Won Chung <jwnchung@umich.edu>
2024-05-06 10:00:41 -0400

    Update pyright.sh

--- .dotmodules/inventory/pyright.sh
+++ .dotmodules/inventory/pyright.sh
@@ -11,6 +11,7 @@ fi
 if ! command -v node 2>&1 > /dev/null; then
   cd /tmp
   curl -LO "https://nodejs.org/dist/v$VERSION/node-v$VERSION-darwin-$ARCH.tar.xz"
+  mkdir -p ~/.local/src
   tar xJf "node-v$VERSION-darwin-$ARCH.tar.xz" -C ~/.local/src
   mv ~/.local/src/node-* ~/.local/src/node
 fi
