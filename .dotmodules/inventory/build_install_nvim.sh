1a4ab01c28e2f1eee27365dc38bdc5958d3838cc
Jae-Won Chung <jwnchung@umich.edu>
2024-06-08 16:47:58 -0400

    Update neovim build script

--- .dotmodules/inventory/build_install_nvim.sh
+++ .dotmodules/inventory/build_install_nvim.sh
@@ -4,13 +4,16 @@ set -e
 
 docker run --rm -v $HOME/.local:$HOME/.local ubuntu:18.04 bash -c \
   "apt-get update &&
-   apt-get install -y --no-install-recommends ninja-build gettext libtool libtool-bin cmake g++ pkg-config unzip curl git doxygen ca-certificates &&
+   apt-get install -y --no-install-recommends ninja-build gettext cmake unzip curl build-essential git ca-certificates &&
+   curl -LO https://github.com/Kitware/CMake/releases/download/v3.29.3/cmake-3.29.3-linux-x86_64.tar.gz &&
+   tar -xzf cmake-3.29.3-linux-x86_64.tar.gz &&
+   ln -s /cmake-3.29.3-linux-x86_64/bin/cmake /usr/local/bin/cmake &&
    cd &&
    git clone https://github.com/neovim/neovim.git &&
    cd neovim &&
    git checkout stable &&
-   make deps &&
    make CMAKE_BUILD_TYPE=Release CMAKE_INSTALL_PREFIX=$HOME/.local &&
+   rm -f ~/.local/bin/nvim ~/.local/lib/nvim ~/.local/share/nvim;
    make CMAKE_INSTALL_PREFIX=$HOME/.local install &&
    chown -R $(id -u):$(id -g) $HOME/.local"
 
