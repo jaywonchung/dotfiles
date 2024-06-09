3921e16033e5b9a2e9c67edc93b7b061b1a0a1a2
Jae-Won Chung <jwnchung@umich.edu>
2024-06-08 20:29:05 -0400

    Fix pyright.sh

--- .dotmodules/inventory/pyright.sh
+++ .dotmodules/inventory/pyright.sh
@@ -8,11 +8,13 @@ else
   ARCH=x64
 fi
 
+KERNEL="$(uname | awk '{print tolower($0)}')"
+
 if ! command -v node 2>&1 > /dev/null; then
   cd /tmp
-  curl -LO "https://nodejs.org/dist/v$VERSION/node-v$VERSION-darwin-$ARCH.tar.xz"
+  curl -LO "https://nodejs.org/dist/v$VERSION/node-v$VERSION-$KERNEL-$ARCH.tar.xz"
   mkdir -p ~/.local/src
-  tar xJf "node-v$VERSION-darwin-$ARCH.tar.xz" -C ~/.local/src
+  tar xJf "node-v$VERSION-$KERNEL-$ARCH.tar.xz" -C ~/.local/src
   mv ~/.local/src/node-* ~/.local/src/node
 fi
 
