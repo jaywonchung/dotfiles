0b029fff5abece7f079488f1758d50dc43f1ae84
Jae-Won Chung <jwnchung@umich.edu>
2024-10-13 21:36:49 -0400

    Update init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -558,14 +558,15 @@ require("lazy").setup({
         " Cursor line (from nightfly)
         highlight! CursorLine  guibg=#092236
 
-        " LSP hover menu dark background (from Tundra)
-        highlight  NormalFloat guibg=#0e1420
         " nvim-cmp autocompletion menu
         highlight  Pmenu       guibg=#0e1420
         
         " diffview.nvim
         highlight  DiffviewFilePanelTitle   guifg=#74c7ed
         highlight  DiffviewFilePanelCounter guifg=#74c7ed
+
+        " lsp_signature.nvim
+        highlight  LspSignatureActiveParameter gui=undercurl
         ]])
       end
     },
@@ -753,33 +754,6 @@ require("lazy").setup({
             end
           end
         end
-        -- local function tab_win_closed(winnr)
-        --   local api = require"nvim-tree.api"
-        --   local tabnr = vim.api.nvim_win_get_tabpage(winnr)
-        --   local bufnr = vim.api.nvim_win_get_buf(winnr)
-        --   local buf_info = vim.fn.getbufinfo(bufnr)[1]
-        --   local tab_wins = vim.tbl_filter(function(w) return w~=winnr end, vim.api.nvim_tabpage_list_wins(tabnr))
-        --   local tab_bufs = vim.tbl_map(vim.api.nvim_win_get_buf, tab_wins)
-        --   if buf_info.name:match(".*NvimTree_%d*$") then            -- closed buffer was nvim tree
-        --     -- Close all nvim tree on :q
-        --     if not vim.tbl_isempty(tab_bufs) then                      -- and was not the last window (not closed automatically by code below)
-        --       api.tree.close()
-        --     end
-        --   else                                                      -- else closed buffer was normal buffer
-        --     if #tab_bufs == 1 then                                    -- if there is only 1 buffer left in the tab
-        --       local last_buf_info = vim.fn.getbufinfo(tab_bufs[1])[1]
-        --       if last_buf_info.name:match(".*NvimTree_%d*$") then       -- and that buffer is nvim tree
-        --         vim.schedule(function ()
-        --           if #vim.api.nvim_list_wins() == 1 then                -- if its the last buffer in vim
-        --             vim.cmd "quit"                                        -- then close all of vim
-        --           else                                                  -- else there are more tabs open
-        --             vim.api.nvim_win_close(tab_wins[1], true)             -- then close only the tab
-        --           end
-        --         end)
-        --       end
-        --     end
-        --   end
-        -- end
 
         vim.api.nvim_create_autocmd("WinClosed", {
           callback = function ()
@@ -918,6 +892,27 @@ require("lazy").setup({
               return " " .. icon .. " " .. count
           end,
         },
+      },
+    },
+    {
+      "epwalsh/obsidian.nvim",
+      version = "*",
+      lazy = true,
+      ft = "markdown",
+      dependencies = {
+        "nvim-lua/plenary.nvim",
+        "hrsh7th/nvim-cmp",
+        "nvim-telescope/telescope.nvim",
+        "nvim-treesitter/nvim-treesitter",
+      },
+      opts = {
+        workspaces = {
+          {
+            name = "notes",
+            path = "~/Google/Sync/Obsidian/notes",
+          },
+        },
+        ui = { enable = false },
       },
     },
     -- Language server protocol
@@ -1135,12 +1130,13 @@ require("lazy").setup({
         notification = { window = { winblend = 0 } },
       },
     },
-    -- {
-    --   'ray-x/lsp_signature.nvim',
-    --   opts = {
-    --     hint_prefix = "@",
-    --   },
-    -- },
+    {
+      'ray-x/lsp_signature.nvim',
+      opts = {
+        hint_enable = false,
+        hint_inline = function() return false end,
+      },
+    },
     {
       "liuchengxu/vista.vim",
       init = function()
@@ -1170,7 +1166,7 @@ require("lazy").setup({
       build = ':TSUpdate',
       config = function()
         require'nvim-treesitter.configs'.setup {
-          ensure_installed = { "c", "cpp", "python", "rust", "go", "vim", "vimdoc", "lua", "zig", "markdown" },
+          ensure_installed = { "c", "cpp", "python", "rust", "go", "vim", "vimdoc", "lua", "zig", "markdown", "markdown_inline" },
           highlight = {
             enable = true,
           },
