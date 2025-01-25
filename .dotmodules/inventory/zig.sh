32e8845907cc15607935446656a2d21c0beac960
Jae-Won Chung <jwnchung@umich.edu>
2025-01-25 17:21:32 -0500

    Add zig.sh in inventory

---
+++ .dotmodules/inventory/zig.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+set -ev
+
+VERSION="${VERSION:-0.13.0}"
+
+unamestr="$(uname)"
+if [[ "$unamestr" == "Darwin" ]]; then
+  OS=macos
+elif [[ "$unamestr" == "Linux" ]]; then
+  OS=linux
+else
+  echo "I never thought about $unamestr"
+  exit 1
+fi
+archstr="$(uname -m)"
+if [[ "$archstr" == "x86_64" ]]; then
+  ARCH=x86_64
+elif [[ "$archstr" == "arm64" ]]; then
+  ARCH=aarch64
+else
+  echo "I never thought about $archstr"
+  exit 1
+fi
+
+cd /tmp
+curl -LO "https://ziglang.org/download/0.13.0/zig-$OS-$ARCH-$VERSION.tar.xz"
+rm -r ~/.local/zig || true
+tar xf "zig-$OS-$ARCH-$VERSION.tar.xz"
+mv "zig-$OS-$ARCH-$VERSION" ~/.local/zig
+rm "zig-$OS-$ARCH-$VERSION.tar.xz"
+ln -s "$HOME/.local/zig/zig" "$HOME/.local/bin/zig"
+
+curl -LO "https://github.com/zigtools/zls/releases/download/$VERSION/zls-$ARCH-$OS.tar.xz"
+tar xf "zls-$ARCH-$OS.tar.xz"
+mv zls ~/.local/bin
+rm "zls-$ARCH-$OS.tar.xz"
