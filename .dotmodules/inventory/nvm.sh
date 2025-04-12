c7b705cd3143954a53bce188fad581dcaddf798c
Jae-Won Chung <jwnchung@umich.edu>
2025-04-11 23:03:28 -0400

    Update inventory scripts

--- .dotmodules/inventory/nvm.sh
+++ .dotmodules/inventory/nvm.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.2/install.sh | bash
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+nvm install node
+nvm alias default node
--- .dotmodules/inventory/texlab.sh
+++ .dotmodules/inventory/texlab.sh
@@ -1,9 +1,15 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+if [[ $(uname -m) = arm64 ]]; then
+  ARCH=aarch64
+else
+  ARCH=x86_64
+fi
 
 mkdir -p "$HOME/.local/bin"
 
 cd /tmp
-wget https://github.com/latex-lsp/texlab/releases/latest/download/texlab-x86_64-macos.tar.gz
-tar xzf texlab-x86_64-macos.tar.gz
-rm texlab-x86_64-macos.tar.gz
+wget https://github.com/latex-lsp/texlab/releases/latest/download/texlab-$ARCH-macos.tar.gz
+tar xzf texlab-$ARCH-macos.tar.gz
+rm texlab-$ARCH-macos.tar.gz
 mv texlab "$HOME/.local/bin"
