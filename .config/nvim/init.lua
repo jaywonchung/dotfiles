1b5cc86585ff701ca19926986f2f89e699c286b2
Jae-Won Chung <jwnchung@umich.edu>
2025-07-11 14:04:10 -0700

    Update init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -413,10 +413,14 @@ require("lazy").setup({
         if pcall(require("copilot.auth").get_creds) then
           require('avante').setup({
             provider = "copilot",
-            copilot = {
-              model = "claude-3.5-sonnet",
-              temperature = 0,
-              max_tokens = 8192,
+            providers = {
+              copilot = {
+                model = "claude-sonnet-4",
+                -- extra_request_body = {
+                --   temperature = 0,
+                --   max_tokens = 8192,
+                -- },
+              },
             },
             mappings = {
               diff = {
@@ -1287,14 +1291,14 @@ require("lazy").setup({
     },
     {
       'stevearc/aerial.nvim',
-      keys = {
-        { '<Leader>t', ':AerialToggle right<CR>', silent = true },
-      },
+      ft = { "c", "cpp", "python", "rust", "go", "zig" },
       dependencies = {
          "nvim-treesitter/nvim-treesitter",
          "nvim-tree/nvim-web-devicons"
       },
       config = function()
+        vim.keymap.set("n", "<Leader>t", ":AerialToggle right<CR>", { silent = true, desc = "Toggle Aerial" })
+
         local function get_scroll_source_fn(key)
           return function()
             local source_win = require("aerial.util").get_winids(0)
@@ -1373,6 +1377,117 @@ require("lazy").setup({
         }
       end
     },
+    {
+      "bngarren/checkmate.nvim",
+      ft = "markdown",
+      opts = {
+        archive = {
+          heading = {
+            title = "Completed",
+            level = 1
+          },
+        },
+        keys = {
+          ["<leader>tt"] = {
+            rhs = "<cmd>Checkmate toggle<CR>",
+            desc = "Toggle todo item",
+            modes = { "n", "v" },
+          },
+          ["<leader>tc"] = {
+            rhs = "<cmd>Checkmate check<CR>",
+            desc = "Set todo item as checked (done)",
+            modes = { "n", "v" },
+          },
+          ["<leader>tu"] = {
+            rhs = "<cmd>Checkmate uncheck<CR>",
+            desc = "Set todo item as unchecked (not done)",
+            modes = { "n", "v" },
+          },
+          ["<leader>tn"] = {
+            rhs = "<cmd>Checkmate create<CR>",
+            desc = "Create todo item",
+            modes = { "n", "v" },
+          },
+          ["<leader>tR"] = {
+            rhs = "<cmd>Checkmate remove_all_metadata<CR>",
+            desc = "Remove all metadata from a todo item",
+            modes = { "n", "v" },
+          },
+          ["<leader>ta"] = {
+            rhs = "<cmd>Checkmate archive<CR>",
+            desc = "Archive checked/completed todo items (move to bottom section)",
+            modes = { "n" },
+          },
+          ["<leader>tv"] = {
+            rhs = "<cmd>Checkmate metadata select_value<CR>",
+            desc = "Update the value of a metadata tag under the cursor",
+            modes = { "n" },
+          },
+          ["<leader>t]"] = {
+            rhs = "<cmd>Checkmate metadata jump_next<CR>",
+            desc = "Move cursor to next metadata tag",
+            modes = { "n" },
+          },
+          ["<leader>t["] = {
+            rhs = "<cmd>Checkmate metadata jump_previous<CR>",
+            desc = "Move cursor to previous metadata tag",
+            modes = { "n" },
+          },
+        },
+        metadata = {
+          priority = {
+            style = function(context)
+              local value = context.value:lower()
+              if value == "high" then
+                return { fg = "#ff5555", bold = true }
+              elseif value == "medium" then
+                return { fg = "#ffb86c" }
+              elseif value == "low" then
+                return { fg = "#8be9fd" }
+              else -- fallback
+                return { fg = "#8be9fd" }
+              end
+            end,
+            get_value = function()
+              return "medium" -- Default priority
+            end,
+            choices = function()
+              return { "low", "medium", "high" }
+            end,
+            key = "<leader>tp",
+            sort_order = 10,
+            jump_to_on_insert = "value",
+            select_on_insert = true,
+          },
+          -- Example: A @started tag that uses a default date/time string when added
+          started = {
+            aliases = { "init" },
+            style = { fg = "#9fd6d5" },
+            get_value = function()
+              return tostring(os.date("%m/%d/%y %H:%M"))
+            end,
+            key = "<leader>ts",
+            sort_order = 20,
+          },
+          -- Example: A @done tag that also sets the todo item state when it is added and removed
+          done = {
+            aliases = { "completed", "finished" },
+            style = { fg = "#96de7a" },
+            get_value = function()
+              return tostring(os.date("%m/%d/%y %H:%M"))
+            end,
+            key = "<leader>td",
+            on_add = function(todo_item)
+              require("checkmate").set_todo_item(todo_item, "checked")
+            end,
+            on_remove = function(todo_item)
+              require("checkmate").set_todo_item(todo_item, "unchecked")
+            end,
+            sort_order = 30,
+          },
+        },
+      }
+    }
   },
   ui = { custom_keys = {}, },
   readme = { enabled = false },
