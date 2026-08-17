8a2dc58d3e9b7876da8ec751774a4b1df8b94298
Jae-Won Chung <jaewon.chung.cs@gmail.com>
2026-08-16 23:38:56 -0400

    Update install scripts

--- .dotmodules/inventory/go.sh
+++ .dotmodules/inventory/go.sh
@@ -1,7 +1,24 @@
 #!/usr/bin/env bash
 
-VERSION=${VERSION:-1.21.5}
+set -e
+
+VERSION=${VERSION:-1.26.3}
+
+if [[ $(uname -m) = arm64 ]]; then
+  ARCH=arm64
+else
+  ARCH=amd64
+fi
+
+unamestr="$(uname)"
+if [[ "$unamestr" == "Darwin" ]]; then
+  filename="go$VERSION.darwin-$ARCH.tar.gz"
+elif [[ "$unamestr" == "Linux" ]]; then
+  filename="go$VERSION.linux-$ARCH.tar.gz"
+fi
+
+rm -rf "$HOME/.local/go" || true
 
 cd /tmp
-curl -LO "https://go.dev/dl/go$VERSION.linux-amd64.tar.gz"
-tar -C "$HOME/.local" -xzf "go$VERSION.linux-amd64.tar.gz"
+curl -LO "https://go.dev/dl/$filename"
+tar -C "$HOME/.local" -xzf "$filename"
